cdb42be88514004e4efb91cab039df92afa3316e
khalidharbi <ka201od7@gmail.com>
2025-10-29 07:25:22 +0300

    edit

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 
     backend "azurerm" {
    resource_group_name = "khalid-rg-tfstate"
-   storage_account_name = "khalidstoragetysb7o"
+   storage_account_name = "khalidstorage0sf7rp"
    container_name = "tfstate"
    key = "terraform.tfstate"
   }
